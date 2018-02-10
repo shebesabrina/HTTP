eb2a13c54807f42e3d8b9d72f95718685fd1b5f0
Sabrina Robinson <sabrinarob@gmail.com>
2018-02-10 14:26:05 -0700

    Adding supporting paths depending on what the client inputs.

--- http.rb
+++ http.rb
@@ -1,31 +1,62 @@
 require 'socket'
-tcp_server = TCPServer.new(9292)
+require 'pry'
 
-count = 0
+class TCPServer
 
-loop {
-  client = tcp_server.accept
+  tcp_server = TCPServer.new(9292)
 
-  puts "Ready for a request"
-  request_lines = []
-  while line = client.gets and !line.chomp.empty?
-    request_lines << line.chomp
-  end
+  count = 0
 
-  puts "Got this request:"
-  puts request_lines.inspect
-  puts "Sending response."
-  response = "<h1>Hello, World! (#{count += 1})</h1>"
-  output = "<html><head></head><body>#{response}</body></html>"
-  headers = ["http/1.1 200 ok",
-            "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
-            "server: ruby",
-            "content-type: text/html; charset=iso-8859-1",
-            "content-length: #{output.length}\r\n\r\n"].join("\r\n")
-  client.puts headers
-  client.puts output
+  loop {
+    client = tcp_server.accept
 
-  puts ["Wrote this response:", headers, output].join("\n")
-  client.close
-  puts "\nResponse complete, exiting."
-}
+    puts "Ready for a request"
+    request_lines = []
+    while line = client.gets and !line.chomp.empty?
+      request_lines << line.chomp
+    end
+
+    puts "Got this request:"
+    puts request_lines.inspect
+    puts "Sending response."
+    request_route_info = request_lines[0].split(" ")
+    host_info = request_lines[1].split(" ")[1].split(":")
+    path = request_route_info[1]
+    response = ""
+    time = Time.now.strftime("%I:%M%p on %A, %B, %Y")#need to create spaces between D/M/Y.
+
+    if path == "/"
+      response = "<pre>
+        Verb: #{request_route_info[0]}
+        Path: #{path}
+        Protocol: #{request_route_info[2]}
+        Host: #{host_info[0]}
+        Port: #{host_info[1]}
+        Origin: #{host_info[0]}
+        Accept: #{request_lines[6].split(" ")[1]}
+        </pre>"
+    elsif path == "/hello"
+      response = "<pre>Hello, World! (#{count += 1})</pre>"
+    elsif path == "/datetime"
+      response = time
+    elsif path == "/shutdown"
+      "Total Requests: #{}" #need to get the total number of the loop.
+    end
+
+
+    output = "<html><head></head><body>#{response}</body></html>"
+    headers = ["http/1.1 200 ok",
+              "date: #{time}",
+              "server: ruby",
+              "content-type: text/html; charset=iso-8859-1",
+              "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+
+
+    client.puts headers
+    client.puts output
+
+    puts ["Wrote this response:", headers, output].join("\n")
+    client.close
+    puts "\nResponse complete, exiting."
+  }
+end
